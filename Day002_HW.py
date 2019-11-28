33e6a60e88df32f2b1bab8c26e3ca9617157f68d
fjufirefox <57890671+fjufirefox@users.noreply.github.com>
2019-11-28 17:06:19 +0800

    Day002_HW

--- Day002_HW.py
+++ Day002_HW.py
@@ -2,10 +2,54 @@
 #
 #根據範例檔案的結果：
 #
+import csv
+from array import array
+
+folderpath = "Data"
+filename = "example.csv"
+filepath = "./" + folderpath + "/" + filename
+
+# 列出CSV全部的資料
+# with open(filepath,
+#           mode='r',
+#           buffering=-1,
+#           encoding='UTF-8',
+#           errors=None,
+#           newline=None,
+#           closefd=True) as cvsfile:
+#     rows = csv.reader(cvsfile)
+#     for row in rows:
+#         print(row)
+
 #1. 取出班次一的每一個時間，印出來就好
 #
+# with open(filepath,
+#           mode='r',
+#           buffering=-1,
+#           encoding='UTF-8',
+#           errors=None,
+#           newline=None,
+#           closefd=True) as cvsfile:
+#     rows = csv.reader(cvsfile)
+#     for row in rows:
+#         print(row[5])
+
 #2. 將班次一的每一個時間用一個變數保存
 #
+data = array()
+with open(filepath,
+          mode='r',
+          buffering=-1,
+          encoding='UTF-8',
+          errors=None,
+          newline=None,
+          closefd=True) as cvsfile:
+
+    rows = csv.reader(filepath)
+    for row in rows:
+        data.append(row[5])
+print(data)
+
 #3. 將所有班次和其每一個時間用一個變數保存
 #
 #(Hint： 2&3 要想一下用什麼的資料型態做整理比較適合)
